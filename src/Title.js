4c76e9670e675162d718233b2cc459786fe13025
Drake Bott <bott.da@gmail.com>
2018-10-06 21:20:25 -0600

    first commit

---
+++ src/Title.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Title.css';
+
+const Title = () => {
+  return (
+    <div className='Title'>
+      <div className='tint'> </div>
+      <div className='titleText'>
+        <h1><span className='orange'>ORANGE</span>SQUEEZERS</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Title;
